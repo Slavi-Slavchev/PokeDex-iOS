f4637b67985cbd07dfc47cd996ae2cf930b3f414
Slavi Slavchev <slavi.lns@gmail.com>
2017-01-07 11:58:13 +0200

    add base UI

---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by Slavi Slavchev on 1/7/17.
+//  Copyright © 2017 3Web Studio - Slavi Slavchev. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
