a95d774cc7d6cceaf3981764826f12a6c50a46d1
Slavi Slavchev <slavi.lns@gmail.com>
2017-01-07 11:19:55 +0200

    Initial Commit

---
+++ PokeDex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by Slavi Slavchev on 1/7/17.
+//  Copyright © 2017 3Web Studio - Slavi Slavchev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
