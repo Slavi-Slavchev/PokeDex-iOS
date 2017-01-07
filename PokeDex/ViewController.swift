a47b3a9f59cef170a7510485e3acc387c5dda08c
Slavi Slavchev <slavi.lns@gmail.com>
2017-01-08 00:55:55 +0200

    add Audio Player and Pokemon font

--- PokeDex/ViewController.swift
+++ PokeDex/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,
 UICollectionViewDelegateFlowLayout{
@@ -14,6 +15,7 @@ UICollectionViewDelegateFlowLayout{
     @IBOutlet weak var collection: UICollectionView!
     
     var pokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,10 +24,27 @@ UICollectionViewDelegateFlowLayout{
         collection.delegate = self
         
         parsePokemonCSV()
+        initAudio()
 
-        
     }
     
+    
+    func initAudio(){
+        
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+        
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+            
+            
+        } catch let err as NSError{
+            print(err.debugDescription)
+        }
+    }
     
     func parsePokemonCSV(){
         let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")
@@ -86,6 +105,18 @@ UICollectionViewDelegateFlowLayout{
         return CGSize (width: 105, height: 105)
     }
     
+    
+    @IBAction func musicBtnPressed(_ sender: UIButton) {
+        
+        if musicPlayer.isPlaying{
+            musicPlayer.pause()
+            sender.alpha = 0.2
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+        
+    }
     
    
 
